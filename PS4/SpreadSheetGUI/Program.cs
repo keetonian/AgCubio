cb660af602e8ebac651d7595b9180bbdfd955b3c
Daniel Avery <daniel.avery@utah.edu>
2015-11-05 10:47:58 -0700

    copy everything over from previous repository

---
+++ PS4/SpreadSheetGUI/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpreadSheetGUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+
+
+    class SpreadsheetAppContext : ApplicationContext
+    {
+        // Number of open forms
+        private int formCount = 0;
+
+        // Singleton ApplicationContext
+        private static SpreadsheetAppContext appContext;
+
+        /// <summary>
+        /// Private constructor for singleton pattern
+        /// </summary>
+        private SpreadsheetAppContext()
+        {
+        }
+
+        /// <summary>
+        /// Returns the one DemoApplicationContext.
+        /// </summary>
+        public static SpreadsheetAppContext getAppContext()
+        {
+            if (appContext == null)//KH- NEED TO RESEARCH THIS
+            {
+                appContext = new SpreadsheetAppContext();
+            }
+            return appContext;
+        }
+
+        /// <summary>
+        /// Runs the form
+        /// </summary>
+        public void RunForm(Form form)
+        {
+            // One more form is running
+            formCount++;
+
+            // When this form closes, we want to find out
+            form.FormClosed += (o, e) => { if (--formCount <= 0) ExitThread(); };
+
+            // Run the form
+            form.Show();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+    this is the demo code:
+
+
+
+    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace SS
+{
+    /// <summary>
+    /// Keeps track of how many top-level forms are running
+    /// </summary>
+    class DemoApplicationContext : ApplicationContext
+    {
+        // Number of open forms
+        private int formCount = 0;
+
+        // Singleton ApplicationContext
+        private static DemoApplicationContext appContext;
+
+        /// <summary>
+        /// Private constructor for singleton pattern
+        /// </summary>
+        private DemoApplicationContext()
+        {
+        }
+
+        /// <summary>
+        /// Returns the one DemoApplicationContext.
+        /// </summary>
+        public static DemoApplicationContext getAppContext()
+        {
+            if (appContext == null)
+            {
+                appContext = new DemoApplicationContext();
+            }
+            return appContext;
+        }
+
+        /// <summary>
+        /// Runs the form
+        /// </summary>
+        public void RunForm(Form form)
+        {
+            // One more form is running
+            formCount++;
+
+            // When this form closes, we want to find out
+            form.FormClosed += (o, e) => { if (--formCount <= 0) ExitThread(); };
+
+            // Run the form
+            form.Show();
+        }
+
+    }
+
+
+    static class Program
+    {
+         /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Start an application context and run one form inside it
+            DemoApplicationContext appContext = DemoApplicationContext.getAppContext(); 
+            appContext.RunForm(new Form1());
+            Application.Run(appContext);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    */
