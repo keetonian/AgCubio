f83c5329890239d2f4fdd2b8f5c4d12eafb1dd36
keetonian <keeton.hodgson@gmail.com>
2015-11-19 20:12:54 -0700

    allow world to parse an xml file for parameters, implements server callback for receiving the player name.

--- AgCubio/Cube/World.cs
+++ AgCubio/Cube/World.cs
@@ -14,67 +14,67 @@ namespace AgCubio
         /// <summary>
         /// Width of the world
         /// </summary>
-        readonly int WIDTH;
+        public readonly int WIDTH;
 
         /// <summary>
         /// Height of the world
         /// </summary>
-        readonly int HEIGHT;
+        public readonly int HEIGHT;
 
         /// <summary>
         /// Number of updates the server attemps to execute per second
         /// </summary>
-        readonly int HEARTBEATS_PER_SECOND;
+        public readonly int HEARTBEATS_PER_SECOND;
 
         /// <summary>
         /// Maximum player speed (for small cube sizes)
         /// </summary>
-        readonly int MAX_SPEED;
+        public readonly int MAX_SPEED;
 
         /// <summary>
         /// Minimum player speed (for large cube sizes)
         /// </summary>
-        readonly int MIN_SPEED;
+        public readonly int MIN_SPEED;
 
         /// <summary>
         /// Scaler for how quickly a player cube loses mass
         /// </summary>
-        readonly double ATTRITION_RATE_SCALER;
+        public readonly double ATTRITION_RATE_SCALER;
 
         /// <summary>
         /// Default mass of food cubes
         /// </summary>
-        readonly int FOOD_MASS;
+        public readonly int FOOD_MASS;
 
         /// <summary>
         /// Starting mass for all players
         /// </summary>
-        readonly int PLAYER_START_MASS;
+        public readonly int PLAYER_START_MASS;
 
         /// <summary>
         /// Maximum total food cubes in the world
         /// </summary>
-        readonly int MAX_FOOD_COUNT;
+        public readonly int MAX_FOOD_COUNT;
 
         /// <summary>
         /// Minimum mass before a player can spit
         /// </summary>
-        readonly int MIN_SPLIT_MASS;
+        public readonly int MIN_SPLIT_MASS;
 
         /// <summary>
         /// How far a cube can be thrown when split
         /// </summary>
-        readonly int MAX_SPLIT_DISTANCE;
+        public readonly int MAX_SPLIT_DISTANCE;
 
         /// <summary>
         /// Maximum total cubes a single player is allowed
         /// </summary>
-        readonly int MAX_SPLIT_COUNT;
+        public readonly int MAX_SPLIT_COUNT;
 
         /// <summary>
         /// Percent overlap before a larger cube eats a smaller
         /// </summary>
-        readonly double ABSORB_PERCENT_COVERAGE;
+        public readonly double ABSORB_PERCENT_COVERAGE;
 
         /// <summary>
         /// Dictionary for storing all the cubes. Uid's map to cubes
@@ -91,6 +91,79 @@ namespace AgCubio
             using (XmlReader reader = XmlReader.Create(filename))
             {
                 //TODO: implement xml file stuff.
+                while(reader.Read())
+                {
+                    if(reader.IsStartElement())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "width":
+                                reader.Read();
+                                int.TryParse(reader.Value,out this.WIDTH);
+                                break;
+
+                            case "height":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.HEIGHT);
+                                break;
+
+                            case "max_split_distance":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.MAX_SPLIT_DISTANCE);
+                                break;
+
+                            case "top_speed":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.MAX_SPEED);
+                                break;
+
+                            case "low_speed":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.MIN_SPEED);
+                                break;
+
+                            case "attrition_rate":
+                                reader.Read();
+                                double.TryParse(reader.Value, out this.ATTRITION_RATE_SCALER);
+                                break;
+
+                            case "food_value":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.FOOD_MASS);
+                                break;
+
+                            case "player_start_mass":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.PLAYER_START_MASS);
+                                break;
+
+                            case "max_food":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.MAX_FOOD_COUNT);
+                                break;
+
+                            case "min_split_mass":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.MIN_SPLIT_MASS);
+                                break;
+
+                            case "absorb_constant":
+                                reader.Read();
+                                double.TryParse(reader.Value, out this.ABSORB_PERCENT_COVERAGE);
+                                break;
+                            /*Good idea: Shall we implement it?
+                            case "max_view_range":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.HEIGHT);
+                                break;
+                            */
+                            case "heartbeats_per_second":
+                                reader.Read();
+                                int.TryParse(reader.Value, out this.HEARTBEATS_PER_SECOND);
+                                break;
+                        }
+                    }
+                }
             }
         }
 
--- AgCubio/Server/Server.cs
+++ AgCubio/Server/Server.cs
@@ -11,6 +11,10 @@ namespace AgCubio
     {
         private Dictionary<string, Socket> NamesSockets;
 
+        private World World;
+
+        
+
         static void Main(string[] args)
         {
             new Server();
@@ -19,6 +23,7 @@ namespace AgCubio
 
         public Server()
         {
+            World = new World(); //Use the file path later.
             NamesSockets = new Dictionary<string, Socket>();
             Network.Server_Awaiting_Client_Loop(new Network.Callback(SetUpClient));
         }
@@ -27,9 +32,40 @@ namespace AgCubio
         {
             NamesSockets.Add(state.data, state.socket);
 
+            //For original UID's: have a counter that counts up and gives unique uid's
+            //When a cube is removed, store the uid in a stack to be reused.
+            //If there is nothing on the stack, increment the counter and use that UID.
+            //IF there is something on the stack, pop it and use that as the uid.
+
+
+            // Generate 2 random starting coords within our world, check if other players are there, then send if player won't get eaten immediately. (helper method)
+            double x, y;
+            FindStartingCoords(out x, out y);
+            Cube cube = new Cube(x,y,GetUid(),false,state.data,World.PLAYER_START_MASS,GetColor(),0);
+            
+
+            //Flow: Get name, send cube, then send all world info, then start the flow back and forth as you receive and send information and requests.
             //Reset the callback
 
             //NEXT SEND CUBE TO CLIENT
         }
+
+
+        private void FindStartingCoords(out x, out y)
+        {
+
+        }
+
+        private int GetUid()
+        {
+
+        }
+
+        private int GetColor()
+        {
+
+        }
+
+
     }
 }
